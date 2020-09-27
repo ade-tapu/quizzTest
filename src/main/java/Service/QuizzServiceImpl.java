5db3343530521b5c315b260fb759aa668720f810
ade-tapu <adelina.ade.constantin@gmail.com>
2020-09-27 12:51:17 +0300

    quizzgetall

--- src/main/java/Service/QuizzServiceImpl.java
+++ src/main/java/Service/QuizzServiceImpl.java
@@ -1,6 +1,10 @@
 package Service;
 
+import com.sda.quizztest.DTO.QuestionDto;
 import com.sda.quizztest.DTO.QuizzDto;
+import com.sda.quizztest.DTO.UserDto;
+import com.sda.quizztest.Model.Login.UserModel;
+import com.sda.quizztest.Model.Quizz.QuestionModel;
 import com.sda.quizztest.Model.Quizz.QuizzModel;
 import com.sda.quizztest.Repository.QuizzRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,12 +23,25 @@ public class QuizzServiceImpl implements QuizzService {
 
         for (QuizzModel quizz : quizzModelList) {
             QuizzDto quizzDto = new QuizzDto();
-            quizzDto.setDescription(quizzDto.getDescription());
-            quizzDto.setQuizz_title(quizzDto.getQuizz_title());
+            quizzDto.setDescription(quizz.getDescription());
+            quizzDto.setQuizz_title(quizz.getQuizz_title());
             quizzDto.setUserDto(quizzDto.getUserDto());
 
+            UserDto userDto = new UserDto();
+            userDto.setName(userDto.getName());
+            userDto.setEmail(userDto.getEmail());
+            userDto.setPassword(userDto.getEmail());
+            quizzDto.setUserDto(userDto);
 
+            List<QuestionModel> questionModelList = quizz.getQuestionModelList();
+            List<QuestionDto> questionDtoList = new ArrayList<>();
+            for (QuestionModel question : questionModelList) {
+                QuestionDto questionDto = new QuestionDto();
+                questionDto.setTitle(question.getTitle());
+                questionDtoList.add(questionDto);
+            }
             quizzDtoList.add(quizzDto);
+
         }
         return quizzDtoList;
     }
