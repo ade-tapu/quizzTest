be2c0d22e063ffd5832c2d4bdaaa46023a7ab4c8
ade-tapu <adelina.ade.constantin@gmail.com>
2020-09-27 13:48:25 +0300

    quizzcontroller

--- src/main/java/com/sda/quizztest/Controller/QuizzController.java
+++ src/main/java/com/sda/quizztest/Controller/QuizzController.java
@@ -1,14 +1,37 @@
 package com.sda.quizztest.Controller;
 
 import Service.QuizzServiceImpl;
+import com.sda.quizztest.DTO.QuizzDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin("http://localhost:4200")
 public class QuizzController {
-    
+
     @Autowired
     private QuizzServiceImpl quizzService;
+
+    @GetMapping("getQuizzes")
+    public ResponseEntity<List<QuizzDto>> getQuizzes() {
+        List<QuizzDto> quizzDtoList = quizzService.getAll();
+        return new ResponseEntity(quizzDtoList, HttpStatus.OK);
+    }
+
+    @PostMapping("addQuizzes")
+    public ResponseEntity addQuizz(@RequestBody QuizzDto quizzDto){
+        quizzService.add(quizzDto);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @GetMapping("getQuizz/{id}")
+    public ResponseEntity<QuizzDto> getQuizz(@PathVariable("id")Long id){
+        QuizzDto quizzDto = quizzService.getById(id);
+        return new ResponseEntity(quizzDto,HttpStatus.OK);
+
+    }
 }
