304d3a83993d09c557845f5850f4070e2e6cc555
ade-tapu <adelina.ade.constantin@gmail.com>
2020-09-27 10:19:09 +0300

    quizzserviceimpl

---
+++ src/main/java/Service/QuizzServiceImpl.java
@@ -0,0 +1,32 @@
+package Service;
+
+import com.sda.quizztest.DTO.QuizzDto;
+
+import java.util.List;
+
+public class QuizzServiceImpl implements QuizzService {
+    @Override
+    public List<QuizzDto> getAll() {
+        return null;
+    }
+
+    @Override
+    public QuizzDto getById(long id) {
+        return null;
+    }
+
+    @Override
+    public Long add(QuizzDto quizz) {
+        return null;
+    }
+
+    @Override
+    public Long delete(QuizzDto quizz) {
+        return null;
+    }
+
+    @Override
+    public Long update(QuizzDto quizz) {
+        return null;
+    }
+}
